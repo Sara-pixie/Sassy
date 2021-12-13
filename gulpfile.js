33ea667074ebeda70bd1a8041b684c1abe6fcb1b
Sara-pixie <78561671+Sara-pixie@users.noreply.github.com>
2021-12-13 14:12:20 +0100

    Purging css

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
     return src("sassyLibrary/**/*.scss")
     .pipe(sass())
+    .pipe(purgecss({ content: ["*.html"] }))
     .pipe(dest("css"))
 }
 
 function watchTask(){
-    watch(["sassyLibrary/**/*.scss"], buildStyles)
+    watch(["sassyLibrary/**/*.scss", "*.html"], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
